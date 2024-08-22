0f4060fdb8fdb6084231aa5309fdeceeaa628e41
tarymee <tarymee@gmail.com>
2024-08-22 16:39:55 +0800

    up

--- serve/index.js
+++ serve/index.js
@@ -5,10 +5,12 @@ liveServer.start({
   host: '0.0.0.0', // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
   root: '', // Set root directory that's being served. Defaults to cwd.
   open: true, // When false, it won't load your browser by default.
-  ignore: '', // comma-separated string for paths to ignore 哪些路径忽略自动刷新
+  ignore: 'scss,my/templates', // comma-separated string for paths to ignore 哪些路径忽略自动刷新
   // file: 'index.html', // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 100, // Waits for all changes, before reloading. Defaults to 0 sec.
   // mount: [['/components', './node_modules']], // Mount a directory to a route.
-  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+  logLevel: 0, // 0 = errors only, 1 = some, 2 = lots
   middleware: [function (req, res, next) { next() }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
 })
+
+console.log('Listening at \x1B[36mhttp://127.0.0.1:12300\x1B[0m')
