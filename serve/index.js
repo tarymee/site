3c8f474bb9e8466b95e463cebc07e9bcfd7a34ed
tarymee <tarymee@gmail.com>
2024-08-22 16:50:43 +0800

    up

--- serve/index.js
+++ serve/index.js
@@ -5,7 +5,7 @@ liveServer.start({
   host: '0.0.0.0', // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
   root: '', // Set root directory that's being served. Defaults to cwd.
   open: true, // When false, it won't load your browser by default.
-  ignore: 'scss,my/templates', // comma-separated string for paths to ignore 哪些路径忽略自动刷新
+  ignore: 'node_modules', // comma-separated string for paths to ignore
   // file: 'index.html', // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 100, // Waits for all changes, before reloading. Defaults to 0 sec.
   // mount: [['/components', './node_modules']], // Mount a directory to a route.
